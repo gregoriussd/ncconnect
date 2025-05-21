c4c9b04f95022b735ccca51d6444c86de5fbb7e1
Gregorius Setiadharma <gregoriussdh@gmail.com>
2025-05-21 22:22:58 +0700

    add docker-compose

--- chat-app/db.ts
+++ chat-app/db.ts
@@ -1,10 +1,17 @@
 import { Client } from "https://deno.land/x/mysql@v2.12.1/mod.ts";
 
+const dbHost = Deno.env.get("DB_HOST") || "localhost";
+const dbPort = parseInt(Deno.env.get("DB_PORT") || "3306", 10);
+const dbUser = Deno.env.get("DB_USER") || "root";
+const dbPassword = Deno.env.get("DB_PASSWORD") || "";
+const dbName = Deno.env.get("DB_NAME") || "chat_app";
+
 const client = await new Client().connect({
-  hostname: "localhost",
-  username: "root", // default user di XAMPP
-  db: "chat_app",
-  password: "", // default-nya kosong di XAMPP (kecuali kamu ubah)
+  hostname: dbHost,
+  port: dbPort,
+  username: dbUser,
+  password: dbPassword,
+  db: dbName,
 });
 
 export default client;
